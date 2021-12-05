18cdb7bbb156c0114dcb37bfa0d800e26d1b3ce2
Eduardo Weiland <eduardo@eduardoweiland.info>
2021-12-04 22:08:46 -0300

    add day 4 part 2

--- 2021/day04/src/Bingo/BingoGame.php
+++ 2021/day04/src/Bingo/BingoGame.php
@@ -4,13 +4,15 @@ declare (strict_types=1);
 
 namespace EW\AoC\Bingo;
 
+use function Functional\contains;
 use function Functional\each;
-use function Functional\first;
+use function Functional\last;
 use function Functional\some;
 
 class BingoGame
 {
     private array $drawnNumbers = [];
+    private array $allWinners = [];
 
     /** @param Board[] $boards */
     public function __construct(private NumberDrawer $numberDrawer, private array $boards)
@@ -22,26 +24,35 @@ class BingoGame
         return some($this->boards, fn ($board) => $board->isWinner());
     }
 
-    public function getWinner(): ?Board
+    public function getLastWinner(): ?Board
     {
-        if (!$this->hasWinner()) {
-            return null;
-        }
-
-        return first($this->boards, fn ($board) => $board->isWinner());
+        return last($this->allWinners);
     }
 
     public function playRound(): void
     {
-        if (!$this->hasWinner()) {
-            $number = $this->numberDrawer->draw();
-            $this->drawnNumbers[] = $number;
-            each($this->boards, fn ($board) => $board->mark($number));
-        }
+        $number = $this->numberDrawer->draw();
+        $this->drawnNumbers[] = $number;
+        each($this->boards, fn ($board) => $board->mark($number));
+        $this->checkWinners();
     }
 
     public function getDrawnNumbers(): array
     {
         return $this->drawnNumbers;
+    }
+
+    public function areThereBoardsStillPlaying(): bool
+    {
+        return some($this->boards, fn ($board) => !$board->isWinner());
+    }
+
+    private function checkWinners(): void
+    {
+        foreach ($this->boards as $board) {
+            if ($board->isWinner() && !contains($this->allWinners, $board, strict: true)) {
+                $this->allWinners[] = $board;
+            }
+        }
     }
 }
