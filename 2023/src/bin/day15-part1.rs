d87e998eb10f508bd9434b4dad1ce7140da482d4
Eduardo Weiland <eduardo@ew.dev.br>
2023-12-24 11:38:57 -0300

    add day 15 part 1

---
+++ 2023/src/bin/day15-part1.rs
@@ -0,0 +1,17 @@
+use std::io::stdin;
+
+fn calc_hash(input: &str) -> u64 {
+    let mut hash = 0;
+
+    input.bytes().for_each(|b| {
+        hash += b as u64;
+        hash *= 17;
+        hash %= 256;
+    });
+
+    hash
+}
+
+fn main() {
+    println!("anwser = {}", stdin().lines().next().unwrap().unwrap().split(',').map(calc_hash).sum::<u64>());
+}
