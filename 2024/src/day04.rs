bc76aea8aa2bf879d603d3be9f206ad755cd6668
Eduardo Weiland <eduardo@ew.dev.br>
2024-12-06 19:14:25 -0300

    add 2024/04 part 1

---
+++ 2024/src/day04.rs
@@ -0,0 +1,116 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day4)]
+fn parse_input(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|l| l.chars().collect()).collect()
+}
+
+#[aoc(day4, part1)]
+pub fn solve_part1(grid: &Vec<Vec<char>>) -> u32 {
+    let mut found = 0;
+
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            Dir::all().iter().for_each(|dir| {
+                let handle = Handle {
+                    grid,
+                    pos: (x as i32, y as i32),
+                    dir: *dir,
+                };
+
+                let letters: Vec<_> = handle.into_iter().take(4).collect();
+                if letters == &['X', 'M', 'A', 'S'] {
+                    found += 1;
+                }
+            });
+        }
+    }
+
+    found
+}
+
+#[derive(Clone, Copy)]
+enum Dir {
+    Right,
+    Left,
+    Up,
+    Down,
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft,
+}
+
+impl Dir {
+    pub fn all() -> Vec<Self> {
+        vec![
+            Dir::Right,
+            Dir::Left,
+            Dir::Up,
+            Dir::Down,
+            Dir::UpRight,
+            Dir::UpLeft,
+            Dir::DownRight,
+            Dir::DownLeft,
+        ]
+    }
+}
+
+struct Handle<'a> {
+    grid: &'a Vec<Vec<char>>,
+    pos: (i32, i32),
+    dir: Dir,
+}
+
+impl Handle<'_> {
+    fn advance(&mut self) {
+        let (x, y) = self.pos;
+        self.pos = match self.dir {
+            Dir::Right => (x + 1, y),
+            Dir::Left => (x - 1, y),
+            Dir::Down => (x, y + 1),
+            Dir::Up => (x, y - 1),
+            Dir::DownRight => (x + 1, y + 1),
+            Dir::DownLeft => (x - 1, y + 1),
+            Dir::UpRight => (x + 1, y - 1),
+            Dir::UpLeft => (x - 1, y - 1),
+        };
+    }
+}
+
+impl Iterator for Handle<'_> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        self.grid
+            .get(self.pos.0 as usize)
+            .and_then(|row| row.get(self.pos.1 as usize))
+            .copied()
+            .and_then(|res| {
+                self.advance();
+                Some(res)
+            })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+    #[test]
+    fn it_solves_part1() {
+        let answer = solve_part1(&parse_input(EXAMPLE_INPUT));
+        assert_eq!(answer, 18);
+    }
+}
